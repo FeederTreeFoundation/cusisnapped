4d387b9fa28b9381cc482d56db18f78d8ebb0b39
Saige <ogcsideways@aol.com>
2024-03-31 21:54:00 -0400

    update configration

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
+  serverRuntimeConfig: {
+    secret: 'THIS IS USED TO SIGN AND VERIFY JWT TOKENS, REPLACE IT WITH YOUR OWN SECRET, IT CAN BE ANY STRING'
+  },
+  publicRuntimeConfig: {
+    apiUrl: `http${process.env.NEXT_PUBLIC_NODE_ENV === 'local' ? '' : 's'}://${process.env.NEXT_PUBLIC_VERCEL_URL}/api`
+  }
 };
 
 export default nextConfig;
