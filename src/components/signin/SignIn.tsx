d215f663ac18095a6aa9ae8eabf95c7241780748
Saige <ogcsideways@aol.com>
2024-04-01 00:50:19 -0400

    add signin page

---
+++ src/components/signin/SignIn.tsx
@@ -0,0 +1,44 @@
+const SignIn = () => {
+    return (
+        <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+            <div className="sm:mx-auto sm:w-full sm:max-w-sm">''
+                <img className="mx-auto h-10 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Tailwind Logo" />
+                <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-stone-100">Submit your email</h2>
+            </div>
+
+            <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+                <form className="space-y-6" action="#" method="POST">
+                <div>
+                    <label htmlFor="email" className="block text-sm font-medium leading-6 text-stone-100">Email address</label>
+                    <div className="mt-2">
+                    <input id="email" name="email" type="email" autoComplete="email" required className="block w-full rounded-md border-0 py-1.5 text-stone-100 shadow-sm ring-1 ring-inset ring-stone-300 placeholder:text-stone-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                    </div>
+                </div>
+
+                <div>
+                    <div className="flex items-center justify-between">
+                    <label htmlFor="password" className="block text-sm font-medium leading-6 text-stone-100">Password</label>
+                    <div className="text-sm">
+                        <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">Forgot password?</a>
+                    </div>
+                    </div>
+                    <div className="mt-2">
+                    <input id="password" name="password" type="password" autoComplete="current-password" required className="block w-full rounded-md border-0 py-1.5 text-stone-100 shadow-sm ring-1 ring-inset ring-stone-300 placeholder:text-stone-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                    </div>
+                </div>
+
+                <div>
+                    <button type="submit" className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign in</button>
+                </div>
+                </form>
+
+                <p className="mt-10 text-center text-sm text-stone-500">
+                Not a member?
+                <a href="#" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">Start a 14 day free trial</a>
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export { SignIn };
